44a8b7cfb2a9b63e29205faef1621ca8b9839fa4
mohamedmosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-12-05 21:24:29 +0200

    Editing the styling of the grid with flexbox also editing the logic of the game

--- code.js
+++ code.js
@@ -1,59 +1,49 @@
-function createGrid(numberOfSquares){
-    for(let i = 1; i <= numberOfSquares; i++)
-    {
-        divList[i-1] = document.createElement('div');
-        divList[i-1].className = `grid-element`;
-        divList[i-1].id = `${i}`;
-    }
+function createGrid(numberOfSquares) {
+  for (let i = 1; i <= numberOfSquares; i++) {
+    divList[i - 1] = document.createElement("div");
+    divList[i - 1].className = `grid-element`;
+    divList[i - 1].id = `${i}`;
+  }
 
-    divList.forEach(element => {
-        gridContainer.appendChild(element);
-    });
+  divList.forEach((element) => {
+    gridContainer.appendChild(element);
+  });
 
-    divList.forEach(element => {
-        element.addEventListener('mouseover',()=>{
-            element.style.backgroundColor = '#F96167'
-        });
+  divList.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      element.style.backgroundColor = "#F96167";
     });
-    
-    divList.forEach(element => {
-        element.addEventListener('mouseout',()=>{
-            element.style.backgroundColor = '#FBEAEB'
-        });
-    });
+  });
 }
 
-
-const gridContainer = document.createElement('div');
+const gridContainer = document.createElement("div");
 gridContainer.className = "grid-container";
 
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 body.appendChild(gridContainer);
 
 let divList = [];
 
 createGrid(15);
 
-
-let buttonDiv = document.createElement('div');
-buttonDiv.className = 'button-div';
+let buttonDiv = document.createElement("div");
+buttonDiv.className = "button-div";
 body.appendChild(buttonDiv);
 
-let createNewGrid = document.createElement('button');
-createNewGrid.className = 'create-new-grid';
-createNewGrid.textContent = 'Create New Grid'
+let createNewGrid = document.createElement("button");
+createNewGrid.className = "create-new-grid";
+createNewGrid.textContent = "Create New Grid";
 
 buttonDiv.appendChild(createNewGrid);
 
-buttonDiv.addEventListener('click', ()=>{
-    let numberOfSquares = +window.prompt('Enter the number of squares');
-    if(numberOfSquares && numberOfSquares <= 100){
-        divList = [];
-        gridContainer.innerHTML = '';
-        createGrid(numberOfSquares);
-    }
-    if(numberOfSquares > 100)
-    {
-        alert('maximum number of squares is 100')
-    }
+buttonDiv.addEventListener("click", () => {
+  let numberOfSquares = +window.prompt("Enter the number of squares");
+  if (numberOfSquares && numberOfSquares <= 100) {
+    divList = [];
+    gridContainer.innerHTML = "";
+    createGrid(numberOfSquares);
+  }
+  if (numberOfSquares > 100) {
+    alert("maximum number of squares is 100");
+  }
 });
